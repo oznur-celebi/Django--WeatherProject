bc89f7aeecc55576edff82350e0e177fa91aa6b7
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-03 09:26:59 +0100

    weatherapp created

---
+++ weatherapp/views.py
@@ -0,0 +1,26 @@
+# from django.shortcuts import render
+# import requests
+# from decouple import config
+
+# # Create your views here.
+# def index(request) :
+#     url ="https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
+#     city ="Berlin"
+#     response =requests.get(url.format(city, config("API_KEY")))
+#     content =response.json()
+#     print(content)
+#     return render(request, "weatherapp/index.html")
+
+from django.shortcuts import render
+import requests
+from decouple import Config
+
+
+def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
+    city = "Berlin"
+    response = requests.get(url.format(city, Config("API_KEY")))
+    content = response.json()
+    print(content)
+    print(type(content))
+    return render(request, "weatherapp/index.html")
